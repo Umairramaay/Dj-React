d950a60619882779a4ae27d164de4f071fe017a6
Umairramay <umairshabr@gmail.com>
2020-06-19 01:11:45 +0500

    Path Set

---
+++ build/precache-manifest.3bc6fb65648998cd2b23bc819e2d6720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce72f18ed265c31941bee6c26b24226",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24d2ab068a1496159914",
+    "url": "/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "24d2ab068a1496159914",
+    "url": "/static/js/2.19b45228.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.19b45228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe34360fd08fb515dac",
+    "url": "/static/js/main.e81340bb.chunk.js"
+  },
+  {
+    "revision": "d96159fb79fad195664d",
+    "url": "/static/js/runtime-main.2dc790a7.js"
+  }
+]);
